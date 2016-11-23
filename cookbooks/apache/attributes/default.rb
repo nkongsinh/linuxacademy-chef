c16927d0b1cd2e857706f64a1c604c7c3a35b916
nknogsinh <nkongsinh@gmail.com>
2016-11-23 06:26:54 +0000

    Updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["nkongsinh2"] = { "site_title" => "NKongsinh2s websit
 default["apache"]["sites"]["nkongsinh2b"] = { "site_title" => "Nkongsinh2bs websites coming soon", "port" => 80, "domain" => "nkongsinh2b.mylabserver.com" }
 default["apache"]["sites"]["nkongsinh3"] = { "site_title" => "Nkongsinh3 website", "port" => 80, "domain" => "nkongsinh3.mylabserver.com" }
 
+default["author"]["name"] = "nyla"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
